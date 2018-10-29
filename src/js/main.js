4bfab61e1b30d3b38a99a503356481717c8f2a1a
Igor Tarasenko <tarasenko.igr@gmail.com>
2018-10-29 16:06:01 +0200

    add mobile version

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,30 @@
+function slideDetect() {
+    if (document.documentElement.clientWidth < 575) {
+        $('.slider').slick('unslick');
+    } else if (document.documentElement.clientWidth > 575) {
+        $('.slider').slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            speed: 500,
+            prevArrow: '<button type="button" class="slick-prev"></button>',
+            nextArrow: '<button type="button" class="slick-next"></button>'
+        });
+    }
+}
 $(document).ready(function(){
-    $('.slider').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        infinite: true,
-        speed: 500,
-        prevArrow: '<button type="button" class="slick-prev"></button>',
-        nextArrow: '<button type="button" class="slick-next"></button>'
+    var $page = $('html, body');
+    $('a[href*="#"]').click(function() {
+        $page.animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 1000);
+        return false;
+    });
+    $('.close').click(function () {
+       $(this).parent().removeClass('show');
+    });
+    slideDetect();
+    $(window).resize(function() {
+        slideDetect()
     });
 });
